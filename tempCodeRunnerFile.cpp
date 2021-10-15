ace8bcda8207322268c9668fcbe65cf2b17b3b25
RitikaKapre <ritika2051.be20@chitkara.edu.in>
2021-10-15 16:57:26 +0530

    Added priority queue in cpp

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,12 @@
+// bool queue_ll::isempty()
+// {
+//     if (front == NULL)
+//         return true;
+//     else
+//     {
+//         return false;
+//     }
+// }
+// int queue_ll::peek(){
+//     return front->data;
+// }
