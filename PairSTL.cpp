7c179be96575aefb133530df407052f82dae7237
Sumanth Bajjuri <73301202+SumanthBajjuri07@users.noreply.github.com>
2021-10-21 01:03:10 +0530

    Create PairSTL.cpp

---
+++ PairSTL.cpp
@@ -0,0 +1,20 @@
+
+
+
+// CPP program to illustrate pair STL
+#include <iostream>
+#include <utility>
+using namespace std;
+
+int main()
+{
+	pair<int, char> PAIR1;
+
+	PAIR1.first = 100;
+	PAIR1.second = 'G';
+
+	cout << PAIR1.first << " ";
+	cout << PAIR1.second << endl;
+
+	return 0;
+}
